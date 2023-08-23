c0f309b687c53ae9fd6f325d458624f6a58fa541
Sam Elliott <soe1@uakron.edu>
2023-08-22 22:17:31 -0400

    it's taking shape, going to be adding in some logos for the tech I'm working with tomorrow and then think about more UX stuff

--- portfolio-page/src/Components/Description.js
+++ portfolio-page/src/Components/Description.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { SvgIcon, Typography } from '@mui/material'
 
 export default function Description() {
     return (
-        <Typography variant='h5'>
-            i'm a Full-Stack Developer in Cuyahoga Falls, OH!
-        </Typography>
+        <div>
+            <div>
+                <Typography variant='h4'>
+                    Hey, I'm Sam!
+                </Typography>
+                <Typography variant='h3'>
+                    Full-Stack Software Developer
+                </Typography>
+                <Typography variant='h4'>
+                    in Cuyahoga Falls, OH
+                </Typography>
+            </div>
+
+            <div>
+                <Typography variant='subtitle1'>
+                    I'm a software developer who loves learning and is excited about all aspects of modern web development! <br></br> I love pushing myself to work through a project!
+                </Typography>
+            </div>
+
+            <div>
+                <Typography variant='h4'>
+                    <strong> Skills. </strong>
+                </Typography>
+
+                <div>
+                    <div>
+                        <Typography variant='subtitle1'> React </Typography>
+                        <SvgIcon>
+                            <svg
+                                xmlns='https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg'
+                                height="1em"
+                                width="1em"
+                            ></svg>
+                        </SvgIcon>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
     )
 }
 
